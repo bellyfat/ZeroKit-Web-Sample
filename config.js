e146486ffa8bf909781a22cc7a6505ee55186618
Mihály Lengyel <porcellus@gmail.com>
2017-06-02 15:56:55 +0200

    Modified server url to match the sample backend

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * The url of the (sample-) backend. Please mind the lack of / at the end.
  * @type {string}
  */
-window.serverUrl = "https://develsamplebackend.azurewebsites.net";
+window.serverUrl = "http://localhost:3000";
 
 /**
  * The id of the Code Flow client you want to use with this example
